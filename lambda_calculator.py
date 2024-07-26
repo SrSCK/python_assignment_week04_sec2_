750e8b62762305dd9c800ca494c640b42b3f00ef
Sorawat Srisuphantavorn <66011232@kmitl.ac.th>
2024-07-26 12:06:44 +0700

    Assignment 5

---
+++ lambda_calculator.py
@@ -0,0 +1,12 @@
+add = lambda x,y: x + y
+subtract = lambda x,y: x - y
+multiply = lambda x,y: x * y
+divide = lambda x,y: x / y
+
+x = int(9)
+y = int(3)
+print("Calculator")
+print(add(x,y))
+print(subtract(x,y))
+print(multiply(x,y))
+print(divide(x,y))
